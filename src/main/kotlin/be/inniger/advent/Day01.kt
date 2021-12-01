e7d6f5c64a0b035e0b306bf1fa559070d2b3a9b4
Bram Inniger <bram@inniger.be>
2021-12-01 19:42:44 +0100

    D01P02 - Solved

--- src/main/kotlin/be/inniger/advent/Day01.kt
+++ src/main/kotlin/be/inniger/advent/Day01.kt
@@ -2,6 +2,9 @@ package be.inniger.advent
 
 object Day01 {
 
-    fun solveFirst(measurements: List<Int>) =
-        (1..measurements.lastIndex).count { measurements[it] > measurements[it - 1] }
+    fun solveFirst(measurements: List<Int>) = countIncreases(measurements, 1)
+    fun solveSecond(measurements: List<Int>) = countIncreases(measurements, 3)
+
+    private fun countIncreases(measurements: List<Int>, indexJump: Int) =
+        (indexJump..measurements.lastIndex).count { measurements[it] > measurements[it - indexJump] }
 }
--- src/test/kotlin/be/inniger/advent/Day01Test.kt
+++ src/test/kotlin/be/inniger/advent/Day01Test.kt
@@ -7,14 +7,25 @@ import kotlin.test.assertEquals
 class Day01Test {
 
     private val input = readInputFile("01").map(String::toInt)
+    private val sampleMeasurements = listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
 
     @Test
     fun validateFirstSampleInputs() {
-        assertEquals(7, Day01.solveFirst(listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)))
+        assertEquals(7, Day01.solveFirst(sampleMeasurements))
     }
 
     @Test
     fun validateFirstSolution() {
         assertEquals(1466, Day01.solveFirst(input))
+    }
+
+    @Test
+    fun validateSecondSampleInputs() {
+        assertEquals(5, Day01.solveSecond(sampleMeasurements))
+    }
+
+    @Test
+    fun validateSecondSolution() {
+        assertEquals(1491, Day01.solveSecond(input))
     }
 }
