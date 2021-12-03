a3ba28c7316a7aab605fc4aad7430ae09a157c5b
Bram Inniger <bram@inniger.be>
2021-12-03 17:27:40 +0100

    D03P02 - Solved

--- src/main/kotlin/be/inniger/advent/Day03.kt
+++ src/main/kotlin/be/inniger/advent/Day03.kt
@@ -19,6 +19,13 @@ object Day03 {
         return gamma * delta
     }
 
+    fun solveSecond(diagnostics: List<String>): Int {
+        val oxygen = determineRatingRec(diagnostics, { diagnosticDigit, common -> diagnosticDigit == common })
+        val co2 = determineRatingRec(diagnostics, { diagnosticDigit, common -> diagnosticDigit != common })
+
+        return oxygen * co2
+    }
+
     private fun readPower(power: List<Int>, reversed: Boolean) = power.map {
         when {
             (if (reversed) -it else it) > 0 -> '0'
@@ -26,4 +33,20 @@ object Day03 {
             else -> error("Even amounts of 0's and 1's in the input")
         }
     }.joinToString("").toInt(2)
+
+    private tailrec fun determineRatingRec(
+        diagnostics: List<String>, predicate: (Char, Char) -> Boolean, position: Int = 0
+    ): Int = if (diagnostics.size <= 1) diagnostics.single().toInt(2)
+    else {
+        val common = mostCommonValue(diagnostics, position)
+        val newDiagnostics = diagnostics.filter { predicate(it[position], common) }
+
+        determineRatingRec(newDiagnostics, predicate, position + 1)
+    }
+
+    private fun mostCommonValue(diagnostics: List<String>, position: Int): Char {
+        val frequencyMap = diagnostics.map { it[position] }.groupingBy { it }.eachCount()
+
+        return if (frequencyMap['0']!! > frequencyMap['1']!!) '0' else '1'
+    }
 }
--- src/test/kotlin/be/inniger/advent/Day03Test.kt
+++ src/test/kotlin/be/inniger/advent/Day03Test.kt
@@ -7,28 +7,38 @@ import kotlin.test.assertEquals
 class Day03Test {
 
     private val input = readInputFile("03")
+    private val sampleDiagnostics = listOf(
+        "00100",
+        "11110",
+        "10110",
+        "10111",
+        "10101",
+        "01111",
+        "00111",
+        "11100",
+        "10000",
+        "11001",
+        "00010",
+        "01010",
+    )
 
     @Test
     fun validateFirstSampleInputs() {
-        val sampleDiagnostics = listOf(
-            "00100",
-            "11110",
-            "10110",
-            "10111",
-            "10101",
-            "01111",
-            "00111",
-            "11100",
-            "10000",
-            "11001",
-            "00010",
-            "01010",
-        )
         assertEquals(198, Day03.solveFirst(sampleDiagnostics))
     }
 
     @Test
     fun validateFirstSolution() {
         assertEquals(4_174_964, Day03.solveFirst(input))
+    }
+
+    @Test
+    fun validateSecondSampleInputs() {
+        assertEquals(230, Day03.solveSecond(sampleDiagnostics))
+    }
+
+    @Test
+    fun validateSecondSolution() {
+        assertEquals(4_474_944, Day03.solveSecond(input))
     }
 }
