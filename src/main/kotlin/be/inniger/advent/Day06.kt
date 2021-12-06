d1d5e4550f3f4ac1aa890581ddd945ca57301c12
Bram Inniger <bram@inniger.be>
2021-12-06 15:12:31 +0100

    D06P02 - Solved

--- src/main/kotlin/be/inniger/advent/Day06.kt
+++ src/main/kotlin/be/inniger/advent/Day06.kt
@@ -2,8 +2,11 @@ package be.inniger.advent
 
 object Day06 {
 
-    fun solveFirst(lanternFish: List<Int>) =
-        simulate(80, lanternFish.groupingBy { it }.eachCount().mapValues { it.value.toLong() })
+    fun solveFirst(lanternFish: List<Int>) = countFish(80, lanternFish)
+    fun solveSecond(lanternFish: List<Int>) = countFish(256, lanternFish)
+
+    private fun countFish(nrDays: Int, lanternFish: List<Int>) =
+        simulate(nrDays, lanternFish.groupingBy { it }.eachCount().mapValues { it.value.toLong() })
             .values
             .sum()
 
@@ -12,15 +15,15 @@ object Day06 {
         else simulate(
             nrDays - 1,
             mapOf(
-                0 to (lanternFish[1] ?: 0),
-                1 to (lanternFish[2] ?: 0),
-                2 to (lanternFish[3] ?: 0),
-                3 to (lanternFish[4] ?: 0),
-                4 to (lanternFish[5] ?: 0),
-                5 to (lanternFish[6] ?: 0),
-                6 to ((lanternFish[7] ?: 0) + (lanternFish[0] ?: 0)),
-                7 to (lanternFish[8] ?: 0),
-                8 to (lanternFish[0] ?: 0)
+                0 to (lanternFish[1] ?: 0L),
+                1 to (lanternFish[2] ?: 0L),
+                2 to (lanternFish[3] ?: 0L),
+                3 to (lanternFish[4] ?: 0L),
+                4 to (lanternFish[5] ?: 0L),
+                5 to (lanternFish[6] ?: 0L),
+                6 to ((lanternFish[7] ?: 0L) + (lanternFish[0] ?: 0L)),
+                7 to (lanternFish[8] ?: 0L),
+                8 to (lanternFish[0] ?: 0L)
             )
         )
 }
--- src/test/kotlin/be/inniger/advent/Day06Test.kt
+++ src/test/kotlin/be/inniger/advent/Day06Test.kt
@@ -7,15 +7,25 @@ import kotlin.test.assertEquals
 class Day06Test {
 
     private val input = readInputFile("06")[0].split(',').map { it.toInt() }
+    private val sampleLanternFish = listOf(3, 4, 3, 1, 2)
 
     @Test
     fun validateFirstSampleInputs() {
-        val sampleLanternFish = listOf(3, 4, 3, 1, 2)
         assertEquals(5_934L, Day06.solveFirst(sampleLanternFish))
     }
 
     @Test
     fun validateFirstSolution() {
         assertEquals(385_391L, Day06.solveFirst(input))
+    }
+
+    @Test
+    fun validateSecondSampleInputs() {
+        assertEquals(26_984_457_539L, Day06.solveSecond(sampleLanternFish))
+    }
+
+    @Test
+    fun validateSecondSolution() {
+        assertEquals(1_728_611_055_389L, Day06.solveSecond(input))
     }
 }
